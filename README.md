73f47672424a8bbbcf3b99a795fc4d343f7e99f0
Tyler Esselman <tme91302@gmail.com>
2022-09-01 18:02:16 -0700

    updated README and image file

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# js-password-generator-ch-3
+# Javascript Password Generator Challenge 3
+
+# Description
+
+For this challenge, we were tasked with writing the function to generate a random password using Javascript functions. We were given the HTML, CSS, and initial Javascript structure so we were able to focus solely building the function for generating the password. This included prompts for the user asking what length they would like their password to be and what types of characters they'd like to include. Additionally, I built in alerts for the user if they chose a password length outside the accepted bounds and if they declined to choose any group of characters.
+
+# Live Link
+
+[LIVELINK](https://tyleresselman.github.io/js-password-generator-ch-3/)
+
+# Screenshot
+
+![Screenshot](./assets/images/password-generator-screenshot.png)
+
+# Credits
+
+I collaborated with my classmates Joseph Ruiz, Jordan Harris, and TA's Paul Cwik and Samuel Adams on the pseudocode for this app.
+
+# License
+
+The MIT License (MIT)
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,30 +15,39 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
+// Here I'm establishing the container variables that I'll need later to complete the generated password.
+
   var characterBank = "";
   // var guaranteedChar = "";
   var yourPassword = "";
 
   // WHEN I click the button to generate a password
   // THEN I am presented with a series of prompts for password criteria
+  
+  // First off, establishing how long the user would like the password to be including a return in case they choose a character number outside of the limits.
 
-  var upperCase = confirm("Do you want your password to include upper case letters?");
+
+  var howLong = prompt("Hello there. I'm a computer! How long would you like this password to be? It can be anywhere between 8 and 128 characters? What sounds good to you?");
+  
+  if (((parseInt(howLong) >= 8)) && ((parseInt(howLong) <= 128))) {
+    // console.log(howLong);
+    // console.log(characterBank);
+  } else {
+    alert("Sorry, the password length has to be at least 8 characters and no more than 128.");
+    return null;
+  }
+
+    // WHEN prompted for the length of the password
+  // THEN I choose a length of at least 8 characters and no more than 128 characters
+
+  // Now I establish which groups of characters the user would like to include in their password.
+
+  var upperCase = confirm("Nice. Next question. Do you want your password to include upper case letters?");
   var lowerCase = confirm("Do you want your password to include lower case letters?");
   var wantNumbers = confirm("How about numbers? Do you want those in your password?");
   var specialChar = confirm("We've got these special characters too. Want to include those?");
 
-  // WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-
-  var howLong = prompt("Great. How long would you like this password to be? It can be anywhere between 8 and 128 characters? What sounds good to you?");
-    if (((parseInt(howLong) >= 8)) && ((parseInt(howLong) <= 128))) {
-
-      console.log(howLong);
-      console.log(characterBank);
-    } else {
-      alert("Sorry, the password length has to be at least 8 characters and no more than 128.");
-      return null;
-    }
+  // Here are the steps to add the specified groups of characters to the available bank of characters for the password.
 
     if (upperCase) {
       characterBank = characterBank + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -66,10 +75,14 @@ function generatePassword() {
       // guaranteedChar = guaranteedChar + getRandom(characterBank);
     }
 
+    // I included this statement in case the user purposefully or accidentally declines all character group options.
+
     if ((!upperCase) && (!lowerCase) && (!wantNumbers) && (!specialChar)) {
       alert("Sorry, you need to pick at least one character category. Try again.");
       return null;
     }
+
+    // This function ensure the randomness of the selection of characters for the password, based on the bank of available characters.
 
     function getRandom(str) {
       var randomIndex = Math.floor(Math.random()*str.length);
@@ -77,6 +90,8 @@ function generatePassword() {
       // console.log(randomChar);
       return randomChar;
     } 
+
+    // And, finally, this function generates the user's password.
   
 
     for(var i = 0; i < parseInt(howLong); i++) {
@@ -89,6 +104,4 @@ function generatePassword() {
 
   }
 
-
-
-generateBtn.addEventListener("click", writePassword);
+/* The commented sections with a guaranteedChar variable are something I tried to establish, with the advice and guidance of TA Sam, to ensure that there would be a character from each group of characters no matter how short the password. That will be something I look to add in the future. */
